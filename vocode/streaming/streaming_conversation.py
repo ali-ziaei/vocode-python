e42763311116f6c465b6b678f9d7fd73542613d1
Ali Ziaei <ali@tenyx.com>
2023-08-29 11:13:20 -0700

    add conversation_id

--- vocode/streaming/streaming_conversation.py
+++ vocode/streaming/streaming_conversation.py
@@ -345,8 +345,8 @@ class StreamingConversation(Generic[OutputDeviceType]):
                 item.agent_response_tracker.set()
                 self.conversation.logger.debug("Message sent: {}".format(message_sent))
                 if cut_off:
-                    self.conversation.agent.update_last_bot_message_on_cut_off(
-                        message_sent
+                    await self.conversation.agent.update_last_bot_message_on_cut_off(
+                        message_sent, conversation_id=self.conversation.id
                     )
                 if self.conversation.agent.agent_config.end_conversation_on_goodbye:
                     goodbye_detected_task = (

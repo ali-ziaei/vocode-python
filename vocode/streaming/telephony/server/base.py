bf39f588739f418656f06549cd1fe7d9bb42d2c2
Ali Ziaei <ali@tenyx.com>
2023-08-31 15:08:57 -0700

    add audio_config to base

--- vocode/streaming/telephony/server/base.py
+++ vocode/streaming/telephony/server/base.py
@@ -7,6 +7,8 @@ from pydantic import BaseModel, Field
 from vocode.streaming.agent.factory import AgentFactory
 from vocode.streaming.models.agent import AgentConfig
 from vocode.streaming.models.events import RecordingEvent
+from vocode.streaming.models.audio import AudioServiceConfig
+
 from vocode.streaming.models.synthesizer import SynthesizerConfig
 from vocode.streaming.models.transcriber import TranscriberConfig
 from vocode.streaming.synthesizer.factory import SynthesizerFactory
@@ -44,6 +46,7 @@ class AbstractInboundCallConfig(BaseModel, abc.ABC):
     agent_config: AgentConfig
     transcriber_config: Optional[TranscriberConfig] = None
     synthesizer_config: Optional[SynthesizerConfig] = None
+    audio_config: Optional[AudioServiceConfig] = None
 
 
 class TwilioInboundCallConfig(AbstractInboundCallConfig):
@@ -99,16 +102,24 @@ class TelephonyServer:
         self.router.add_api_route("/events", self.events, methods=["GET", "POST"])
         self.logger.info(f"Set up events endpoint at https://{self.base_url}/events")
 
-        self.router.add_api_route("/recordings/{conversation_id}", self.recordings, methods=["GET", "POST"])
-        self.logger.info(f"Set up recordings endpoint at https://{self.base_url}/recordings/{{conversation_id}}")
- 
+        self.router.add_api_route(
+            "/recordings/{conversation_id}", self.recordings, methods=["GET", "POST"]
+        )
+        self.logger.info(
+            f"Set up recordings endpoint at https://{self.base_url}/recordings/{{conversation_id}}"
+        )
+
     def events(self, request: Request):
         return Response()
 
     async def recordings(self, request: Request, conversation_id: str):
         recording_url = (await request.json())["recording_url"]
         if self.events_manager is not None and recording_url is not None:
-            self.events_manager.publish_event(RecordingEvent(recording_url=recording_url, conversation_id=conversation_id))
+            self.events_manager.publish_event(
+                RecordingEvent(
+                    recording_url=recording_url, conversation_id=conversation_id
+                )
+            )
         return Response()
 
     def create_inbound_route(
@@ -156,7 +167,9 @@ class TelephonyServer:
             conversation_id = create_conversation_id()
             await self.config_manager.save_config(conversation_id, call_config)
             return VonageClient.create_call_ncco(
-                base_url=self.base_url, conversation_id=conversation_id, record=vonage_config.record
+                base_url=self.base_url,
+                conversation_id=conversation_id,
+                record=vonage_config.record,
             )
 
         if isinstance(inbound_call_config, TwilioInboundCallConfig):

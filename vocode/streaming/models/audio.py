ed5f659d33101ea09caca3e9b0be1bd593e08992
Ali Ziaei <ali@tenyx.com>
2023-08-31 14:03:25 -0700

    audio service

---
+++ vocode/streaming/models/audio.py
@@ -0,0 +1,19 @@
+from .audio_encoding import AudioEncoding
+from .model import TypedModel
+from enum import Enum
+from vocode.streaming.models.model import BaseModel
+import datetime
+
+
+class AudioServiceType(str, Enum):
+    """audio service types"""
+
+    BASE = "audio_service_base"
+
+
+class AudioServiceConfig(TypedModel, type=AudioServiceType.BASE.value):
+    """Audio service config"""
+
+    sampling_rate: int
+    audio_encoding: AudioEncoding
+    chunk_size: int

74e8aca28e9f453d93e6ae08d054c6ec6c4b8b21
Ali Ziaei <103010578+ali-ziaei@users.noreply.github.com>
2023-09-06 09:57:20 -0700

    Release 2023-09-05 (#7)
    * logging support
    
    * better design
    
    * remove audio logging for now
    
    * add formatter
    
    * wip
    
    * json logger

--- vocode/streaming/utils/events_manager.py
+++ vocode/streaming/utils/events_manager.py
@@ -1,16 +1,22 @@
 from __future__ import annotations
 
 import asyncio
-from typing import List
+import os
+from typing import List, Optional
 
 from vocode.streaming.models.events import Event, EventType
 
 
 class EventsManager:
-    def __init__(self, subscriptions: List[EventType] = []):
+    def __init__(
+        self, subscriptions: List[EventType] = [], log_dir: Optional[str] = None
+    ):
         self.queue: asyncio.Queue[Event] = asyncio.Queue()
         self.subscriptions = set(subscriptions)
         self.active = False
+        self.log_dir = log_dir
+        if log_dir:
+            os.makedirs(log_dir, exist_ok=True)
 
     def publish_event(self, event: Event):
         if event.type in self.subscriptions:

fdf453b096529f273e3f239c9470227f0d10bc3b
Matthew Ceravolo <matthew@tenyx.com>
2023-10-13 10:29:33 -0700

    changing property name

--- vocode/streaming/models/telephony.py
+++ vocode/streaming/models/telephony.py
@@ -28,7 +28,7 @@ class TwilioConfig(BaseModel):
     auth_token: str
     record: bool = False
     recording_url: Optional[str] = None
-    status_callback_url: Optional[str] = None
+    events_url: Optional[str] = None
     extra_params: Optional[Dict[str, Any]] = {}
 
 
@@ -39,6 +39,7 @@ class VonageConfig(BaseModel):
     private_key: str
     record: bool = False
     recording_url: Optional[str] = None
+    events_url: Optional[str] = None
 
 
 class CallEntity(BaseModel):

da071631880db1073e4736e79aa5ebd99e797658
Ali Ziaei <ali@tenyx.com>
2023-10-20 16:53:12 -0700

    wip

--- vocode/streaming/streaming_conversation.py
+++ vocode/streaming/streaming_conversation.py
@@ -696,15 +696,9 @@ class StreamingConversation(Generic[OutputDeviceType]):
                                 await self.conversation.terminate()
 
                 if item.interruption_event.is_set():
-                    print("WE ARE HERE!!!!!!!!!!!")
                     self.conversation.transcriptions_postprocessing_worker.endpoint_threshold = copy.deepcopy(
                         self.conversation.transcriber.transcriber_config.new_endpoint_sec
                     )
-                    print("\n\n\n\n")
-                    print(
-                        self.conversation.transcriptions_postprocessing_worker.endpoint_threshold
-                    )
-                    print("\n\n\n\n")
 
                     await self.conversation.agent.update_last_bot_message_on_cut_off(
                         message_sent, conversation_id=self.conversation.id

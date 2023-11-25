a55035f5a18a68e737e6b294f1040f80bd094ca4
Ali Ziaei <ali@tenyx.com>
2023-11-25 09:12:36 -0800

    add  def get_endpoint_prediction(self, chat, context):

--- vocode/streaming/agent/base_agent.py
+++ vocode/streaming/agent/base_agent.py
@@ -136,6 +136,9 @@ class AbstractAgent(Generic[AgentConfigType]):
         assert len(on_cut_off_messages) > 0
         return random.choice(on_cut_off_messages).text
 
+    async def get_endpoint_prediction(self, chat, context):
+        pass
+
 
 class BaseAgent(AbstractAgent[AgentConfigType], InterruptibleWorker):
     def __init__(

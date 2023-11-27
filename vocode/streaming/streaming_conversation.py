5b95abc8fc9e81f64550b5600f78a8a30c813d3f
Ali Ziaei <ali@tenyx.com>
2023-11-26 19:55:04 -0800

    wip

--- vocode/streaming/streaming_conversation.py
+++ vocode/streaming/streaming_conversation.py
@@ -243,24 +243,24 @@ class StreamingConversation(Generic[OutputDeviceType]):
                 self.conversation.transcriber.unmute()
 
         async def _flush_asr_queue(self):
-            transcription_should_be_sent_to_llm: Optional[Transcription] = None
+            transcription_in_queue: Optional[Transcription] = None
             while True:
                 try:
                     item = (
                         self.conversation.transcriptions_postprocessing_worker.output_queue.get_nowait()
                     )
 
-                    if transcription_should_be_sent_to_llm is None:
-                        transcription_should_be_sent_to_llm = item.payload.transcription
+                    if transcription_in_queue is None:
+                        transcription_in_queue = item.payload.transcription
                     else:
-                        transcription_should_be_sent_to_llm.message = (
-                            transcription_should_be_sent_to_llm.message
+                        transcription_in_queue.message = (
+                            transcription_in_queue.message.strip(".")
                             + " "
                             + item.payload.transcription.message
                         )
                 except asyncio.queues.QueueEmpty:
                     break
-            return transcription_should_be_sent_to_llm
+            return transcription_in_queue
 
         async def publish_asr_result(self):
             if not self.conversation.transcriptions_worker.is_speaking_at:
@@ -270,24 +270,58 @@ class StreamingConversation(Generic[OutputDeviceType]):
                 time.time() - self.conversation.transcriptions_worker.is_speaking_at
             )
 
+            # first manual endpoint point before we send to end point model
             if wait_time >= self.conversation.asr_post_process_endpoint_sec:
-                transcription_should_be_sent_to_llm = await self._flush_asr_queue()
-                if transcription_should_be_sent_to_llm:
-                    event = self.conversation.transcriptions_postprocessing_worker.interruptible_event_factory.create_interruptible_event(
-                        TranscriptionAgentInput(
-                            transcription=transcription_should_be_sent_to_llm,
-                            conversation_id=self.conversation.id,
-                            vonage_uuid=getattr(self.conversation, "vonage_uuid", None),
-                            twilio_sid=getattr(self.conversation, "twilio_sid", None),
-                        ),
-                        is_interruptible=False,
+                transcription_in_queue = await self._flush_asr_queue()
+                if transcription_in_queue:
+                    if (
+                        self.conversation.transcriptions_postprocessing_worker.final_transcription
+                        is None
+                    ):
+                        self.conversation.transcriptions_postprocessing_worker.final_transcription = (
+                            transcription_in_queue
+                        )
+                    else:
+                        self.conversation.transcriptions_postprocessing_worker.final_transcription.message = (
+                            self.conversation.transcriptions_postprocessing_worker.final_transcription.message.strip(
+                                "."
+                            )
+                            + " "
+                            + transcription_in_queue.message
+                        )
+
+                    # model based endpoint pointing
+                    is_endpoint = await self.conversation.agent.get_endpoint_prediction(
+                        self.conversation.transcriptions_postprocessing_worker.final_transcription.message,
+                        self.conversation.id,
                     )
-                    await self.conversation.agent.get_input_queue().put(event)
+
+                    if (
+                        is_endpoint
+                        or wait_time >= self.conversation.agent_endpoint_config.time_out
+                    ):
+                        event = self.conversation.transcriptions_postprocessing_worker.interruptible_event_factory.create_interruptible_event(
+                            TranscriptionAgentInput(
+                                transcription=self.conversation.transcriptions_postprocessing_worker.final_transcription,
+                                conversation_id=self.conversation.id,
+                                vonage_uuid=getattr(
+                                    self.conversation, "vonage_uuid", None
+                                ),
+                                twilio_sid=getattr(
+                                    self.conversation, "twilio_sid", None
+                                ),
+                            ),
+                            is_interruptible=False,
+                        )
+                        await self.conversation.agent.get_input_queue().put(event)
+                        self.conversation.transcriptions_postprocessing_worker.final_transcription = (
+                            None
+                        )
 
                     context = VocodeLogContext(self.conversation.id)
                     log_message = VocodeBaseLogMessage(
                         message=f'ASR: transcription_should_be_sent_to_llm with dynamic endpoint "{self.conversation.asr_post_process_endpoint_sec}"',
-                        text=f'Transcription: "{transcription_should_be_sent_to_llm.message}", Latency: "{transcription_should_be_sent_to_llm.latency}" seconds, and asr post processing endpointing: "{self.conversation.asr_post_process_endpoint_sec}" seconds.',
+                        text=f'Transcription: "{transcription_in_queue.message}", Latency: "{transcription_in_queue.latency}" seconds, and asr post processing endpointing: "{self.conversation.asr_post_process_endpoint_sec}" seconds.',
                     )
                     self.conversation.logger.debug(log_message, context=context)
 
@@ -417,6 +451,7 @@ class StreamingConversation(Generic[OutputDeviceType]):
             self.output_queue = output_queue
             self.conversation = conversation
             self.interruptible_event_factory = interruptible_event_factory
+            self.final_transcription: Optional[Transcription] = None
 
         async def process(self, item: InterruptibleAgentResponseEvent):
             context = VocodeLogContext(self.conversation.id)
@@ -819,6 +854,8 @@ class StreamingConversation(Generic[OutputDeviceType]):
         self.spoken_metadata = SpokenMetaData()
 
         self.agent_filler_config = self.agent.get_agent_config().agent_filler_config
+        self.agent_endpoint_config = self.agent.get_agent_config().agent_endpoint_config
+
         self.num_retry_ask_more_time_in_row = 0
         self.num_retry_speak_up_in_row = 0
 

e9f5061196724d598bbe402e785542e62624a451
Ali Ziaei <ali@tenyx.com>
2023-12-05 14:59:01 -0800

    fix partial

--- vocode/streaming/transcriber/deepgram_transcriber.py
+++ vocode/streaming/transcriber/deepgram_transcriber.py
@@ -266,7 +266,7 @@ class DeepgramTranscriber(BaseAsyncTranscriber[DeepgramTranscriberConfig]):
                     elif top_choice["transcript"] and confidence > 0.0:
                         self.output_queue.put_nowait(
                             Transcription(
-                                message=buffer,
+                                message=top_choice["transcript"],
                                 confidence=confidence,
                                 is_final=False,
                                 latency=cur_min_latency,
